ad8c93b9b390b7265a968c75bbfde67e9de9d800
Nguyễn Thị Thảo <74347588+nguyenthithaodream@users.noreply.github.com>
2021-01-19 09:34:07 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,43 @@
+<div class="menu">
+                <ul class="ul1">
+                    <li><a href="./index_staff.php"><img width="30px" height="20px" src="../anh/home.png"/> Home</a></li>
+                    <li><a href=""><img width="30px" height="20px" src="../anh/phanCong.jpg"/> Phân công giao nhận <span class="icon1"></span></a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=2">Phân công nhận hàng</a></li>
+                            <li><a href="./index_staff.php?xem=3">Phân công giao hàng</a></li>
+                        </ul>
+                    </li>
+                    <li><a href=""><img width="30px" height="20px" src="../anh/quanLyDonGiaoHang.png"/> Quản lý đơn giao hàng</a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=6">Danh sách quản lý đơn hàng</a></li>
+                            <li><a href="./index_staff.php?xem=29">Cập nhật đơn hàng</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./index_staff.php?xem=7"><img width="30px" height="20px" src="../anh/dongTien.jpg"/> Quản lý dòng tiền <span class="icon1"></span></a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=8">Theo dõi tiền đối soát</a></li>
+                            <li><a href="./index_staff.php?xem=9">Sửa thông tin</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./index_staff.php?xem=10"><img width="30px" height="20px" src="../anh/yeuCauGiaoHnag.png"/> Yêu cầu giao hàng <span class="icon1"></span></a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=11">Tạo đơn hàng</a></li>
+                            <li><a href="./index_staff.php?xem=12">Danh sách đơn hàng</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="./index_staff.php?xem=14"><img width="30px" height="20px" src="../anh/quanLyNhanVien.png"/> Quản lý nhân viên <span class="icon1"></span></a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=15">Tạo mới nhân viên</a></li>
+                            <li><a href="./index_staff.php?xem=16">Danh sách nhân viên</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./index_staff.php?xem=17"><img width="30px" height="20px" src="../anh/quanLyTaiKhoan.png"/> Quản lý tài khoản <span class="icon1"></span></a>
+                        <ul class="sub-menu">
+                            <li><a href="./index_staff.php?xem=18">Tạo tài khoản</a></li>
+                            <li><a href="./index_staff.php?xem=19">Danh sách tài khoản</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./index_staff.php?xem=20"><img width="30px" height="20px" src="../anh/baoCao.jpg"/> Báo cáo</a></li>
+                </ul>
+            </div>

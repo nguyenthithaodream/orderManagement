0ad3b9a031e09d62940580e90fa4006d79669c7e
Nguyễn Thị Thảo <74347588+nguyenthithaodream@users.noreply.github.com>
2021-01-19 09:33:16 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="header">
+            <div class="logo">
+                 <img src="../anh/logo.png" style="width: 100%; height: 100%;"/>
+            </div>
+            <div class="add">
+                
+                <span style="color: white; float: right; margin: 0px 30px 0px; font-size: 14px; line-height: 20px;">
+                    <img src="../anh/nguoidung.png" style="width: 40px; height: 20px;"/> &nbsp;
+                    <?php echo $_SESSION['ten'];?>
+                    <br /><a class="a_header" href="./logout.php" style=" color: white; float: right; margin: 0px 25px 0px; font-size: 14px;"> Log out </a>
+                </span> 
+            </div>
+        </div>
